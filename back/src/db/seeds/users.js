5eab9b63289e9c2c9b16fa1bbf97e7b2f6b79e4c
Ibarreche <46689861+TomIbarreche@users.noreply.github.com>
2021-07-09 14:59:40 +0200

    Update users.js

--- back/src/db/seeds/users.js
+++ back/src/db/seeds/users.js
@@ -1,4 +1,3 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('users').del()
@@ -8,5 +7,5 @@ exports.seed = function(knex) {
         {id: 1, user_name: 'admin', first_name: 'Administrator', last_name: 'god', email: 'admin@roadtrip.eu', password:'$2a$12$3xhaxNHbdg35gi0/5PrkUuFwfaPgyIOUBVptTpK4eUlYZfuI/SqFW', phone:'', birthday:'2020-02-12 20:00:00', role_id: 1},
         {id: 2, user_name: 'simpleuser', first_name: 'Simple', last_name: 'user', email: 'simple.user@roadtrip.eu', password:'$2a$12$3xhaxNHbdg35gi0/5PrkUuFwfaPgyIOUBVptTpK4eUlYZfuI/SqFW', phone:'', birthday:'2020-02-12 20:00:00', role_id: 2},
       ]);
-    )};
+    });
 };
