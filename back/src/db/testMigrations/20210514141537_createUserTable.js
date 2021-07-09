9925e94c4fa382a9a727092aea7910a127b3151f
Ibarreche <46689861+TomIbarreche@users.noreply.github.com>
2021-07-09 14:27:04 +0200

    Update 20210514141537_createUserTable.js

--- back/src/db/testMigrations/20210514141537_createUserTable.js
+++ back/src/db/testMigrations/20210514141537_createUserTable.js
@@ -3,8 +3,8 @@ exports.up = function(knex) {
     return knex.schema.createTable('users', table => {
         table.increments('id');
         table.string('user_name').notNullable().unique();
-        table.string('first_name').notNullable();
-        table.string('last_name').notNullable();
+        table.string('first_name');
+        table.string('last_name');
         table.string('email').notNullable().unique();
         table.string('password').notNullable()
         table.string('phone');
